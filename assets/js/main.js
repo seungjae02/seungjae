95978acd468f25a764c10e3f2909a7386299ebb4
Seung Jae Yang <sjyang1126@gmail.com>
2021-08-12 22:13:26 -0400

    work experience added

--- assets/js/main.js
+++ assets/js/main.js
@@ -207,10 +207,10 @@
 			});
 
 	// Main.
-		var $main = $('#main');
+		var $work = $('#main');
 
 		// Thumbs.
-			$main.children('.thumb').each(function() {
+			$work.children('.thumb').each(function() {
 
 				var	$this = $(this),
 					$image = $this.find('.image'), $image_img = $image.children('img'),
@@ -237,7 +237,7 @@
 			});
 
 		// Poptrox.
-			$main.poptrox({
+			$work.poptrox({
 				baseZIndex: 20000,
 				caption: function($a) {
 
@@ -271,11 +271,83 @@
 
 			// Hack: Set margins to 0 when 'xsmall' activates.
 				breakpoints.on('<=xsmall', function() {
-					$main[0]._poptrox.windowMargin = 0;
+					$work[0]._poptrox.windowMargin = 0;
 				});
 
 				breakpoints.on('>xsmall', function() {
-					$main[0]._poptrox.windowMargin = 50;
+					$work[0]._poptrox.windowMargin = 50;
+				});
+
+		// Work.
+		var $work = $('#work');
+
+		// Thumbs.
+			$work.children('.thumb').each(function() {
+
+				var	$this = $(this),
+					$image = $this.find('.image'), $image_img = $image.children('img'),
+					x;
+
+				// No image? Bail.
+					if ($image.length == 0)
+						return;
+
+				// Image.
+				// This sets the background of the "image" <span> to the image pointed to by its child
+				// <img> (which is then hidden). Gives us way more flexibility.
+
+					// Set background.
+						$image.css('background-image', 'url(' + $image_img.attr('src') + ')');
+
+					// Set background position.
+						if (x = $image_img.data('position'))
+							$image.css('background-position', x);
+
+					// Hide original img.
+						$image_img.hide();
+
+			});
+
+		// Poptrox.
+			$work.poptrox({
+				baseZIndex: 20000,
+				caption: function($a) {
+
+					var s = '';
+
+					$a.nextAll().each(function() {
+						s += this.outerHTML;
+					});
+
+					return s;
+
+				},
+				fadeSpeed: 300,
+				onPopupClose: function() { $body.removeClass('modal-active'); },
+				onPopupOpen: function() { $body.addClass('modal-active'); },
+				overlayOpacity: 0,
+				popupCloserText: '',
+				popupHeight: 150,
+				popupLoaderText: '',
+				popupSpeed: 300,
+				popupWidth: 150,
+				selector: '.thumb > a.image',
+				usePopupCaption: true,
+				usePopupCloser: true,
+				usePopupDefaultStyling: false,
+				usePopupForceClose: true,
+				usePopupLoader: true,
+				usePopupNav: true,
+				windowMargin: 50
+			});
+
+			// Hack: Set margins to 0 when 'xsmall' activates.
+				breakpoints.on('<=xsmall', function() {
+					$work[0]._poptrox.windowMargin = 0;
+				});
+
+				breakpoints.on('>xsmall', function() {
+					$work[0]._poptrox.windowMargin = 50;
 				});
 
 })(jQuery);
